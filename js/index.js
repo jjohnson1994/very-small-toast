9c64c985088776622f9edbd629bd197e962a8542
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2018-04-28 17:08:25 +0100

    No need to be a class KISS

--- js/index.js
+++ js/index.js
@@ -5,37 +5,31 @@ const toastClass = 'vsToast';
 
 let toastContainer = null;
 
-export default class Toast {
-  constructor() {
-    /**
-     * Either Create the toast container
-     * or use one already in the DOM 
-     */
-    if (!document.querySelector(`#${toastContainerId}`)) {
-      toastContainer = document.createElement('div');
-      toastContainer.id = toastContainerId;
-    } else {
-      toastContainer = document.querySelector(`#${toastContainerId}`);
-    }
-    
-    document.body.appendChild(toastContainer);
+/**
+ * Either Create the toast container
+ * or use one already in the DOM 
+ */
+if (!document.querySelector(`#${toastContainerId}`)) {
+  toastContainer = document.createElement('div');
+  toastContainer.id = toastContainerId;
+} else {
+  toastContainer = document.querySelector(`#${toastContainerId}`);
+}
 
-    window.$toast = this.toast;
-  }
+document.body.appendChild(toastContainer);
 
-  toast(text) {
-    // Create the toast
-    let toast = document.createElement('div');
-    toast.classList.add(toastClass);
-    let toastText = document.createElement('span');
-    toastText.innerHTML = text;
-    toast.appendChild(toastText);
-  
-    // remove the last toast if it's there
-    if (toastContainer.childNodes[0])
-      toastContainer.removeChild(toastContainer.childNodes[0]);
-  
-    // add the new toast
-    toastContainer.appendChild(toast);
-  }
+window.$toast = function(text) {
+  // Create the toast
+  let toast = document.createElement('div');
+  toast.classList.add(toastClass);
+  let toastText = document.createElement('span');
+  toastText.innerHTML = text;
+  toast.appendChild(toastText);
+
+  // remove the last toast if it's there
+  if (toastContainer.childNodes[0])
+    toastContainer.removeChild(toastContainer.childNodes[0]);
+
+  // add the new toast
+  toastContainer.appendChild(toast);
 }
